ea7273c4d9fdad05a53d9e667b5b81a61f612fdf
Nikos Karampatziakis <just.nikos@gmail.com>
2022-12-15 11:00:06 -0800

    Add files via upload

---
+++ ts-dynload/TreeContextualFilter.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.score = exports.version = void 0;
+function version() {
+    return "3";
+}
+exports.version = version;
+function score(input) {
+    return [0.75, 0.25];
+}
+exports.score = score;
